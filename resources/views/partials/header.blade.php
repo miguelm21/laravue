adf6e17780536f7f18b0139091c5a72533a5755b
Miguel <miguel.lm21@gmail.com>
2018-03-15 20:53:59 -0400

    Arreglo de errores minimos

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<title>Club de Beneficios 2</title>
+<title>Club de Beneficios</title>
 <meta name="description" content="This is an example of a meta description. This will often show up in search results.">
 <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
 <!-- Fonts -->
